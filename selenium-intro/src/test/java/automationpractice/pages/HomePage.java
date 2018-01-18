40848a64527bdaa4043606bd369b4bb7d5441760
ale <ale.nadasan@gmail.com>
2018-01-18 13:56:50 +0200

    added add to cart tests

--- selenium-intro/src/test/java/automationpractice/pages/HomePage.java
+++ selenium-intro/src/test/java/automationpractice/pages/HomePage.java
@@ -2,8 +2,12 @@ package automationpractice.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import resources.PageBase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Ale on 15/01/18.
@@ -17,6 +21,18 @@ public class HomePage extends PageBase{
     private WebElement searchField;
     @FindBy(name = "submit_search")
     private WebElement searchButton;
+
+    @FindBy(xpath = "//div[@class='shopping_cart']/a")
+    private WebElement cartButton;
+    @FindBy(className = "cart_block_product_name")
+    private List<WebElement> cartItems;
+
+    @FindBy(xpath = "//ul[@id='homefeatured']//a[@class='product_img_link']")
+    private List<WebElement> popularProducts;
+    @FindBy(xpath = "//ul[@id='homefeatured']//span[text()='Add to cart']")
+    private List<WebElement> addToCartButtonsForPopularProducts;
+    @FindBy(xpath = "//span[@title = 'Continue shopping']")
+    private WebElement continueShoppingModalButton;
 
     public HomePage(WebDriver driver) {
         super(driver);
@@ -36,5 +52,42 @@ public class HomePage extends PageBase{
         searchButton.click();
 
         return new ResultsPage(driver);
+    }
+
+    public List<String> getCartItems() {
+        waitForElementToBeVisible(cartButton);
+        new Actions(driver).moveToElement(cartButton).perform();
+        waitForElementsToBeVisible(cartItems);
+
+        List<String> itemNames = new ArrayList<String>();
+        for (WebElement cartItem : cartItems)
+            itemNames.add(cartItem.getAttribute("title"));
+
+        return itemNames;
+    }
+
+    public HomePage addAllPopularProductsToCart() {
+        waitForElementsToBeVisible(popularProducts);
+
+        Actions actions = new Actions(driver);
+        for (WebElement product : popularProducts) {
+            actions.moveToElement(product).perform();
+            waitForElementToBeVisible(addToCartButtonsForPopularProducts.get(popularProducts.indexOf(product)));
+            addToCartButtonsForPopularProducts.get(popularProducts.indexOf(product)).click();
+
+            waitForElementToBeVisible(continueShoppingModalButton);
+            continueShoppingModalButton.click();
+        }
+
+        return new HomePage(driver);
+    }
+
+    public List<String> getPopularItemsNames() {
+        List<String> itemNames = new ArrayList<String>();
+
+        for (WebElement product : popularProducts)
+            itemNames.add(product.getAttribute("title"));
+
+        return itemNames;
     }
 }
